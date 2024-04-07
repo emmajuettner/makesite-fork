d784ad954f65728d05094964417796dcabc1bc26
Emma Juettner <22248856+emmajuettner@users.noreply.github.com>
2024-04-06 21:40:37 -0500

    Add code for CSS Naked day

--- static/js/theme.js
+++ static/js/theme.js
@@ -2,17 +2,26 @@ function theme_set(theme_name) {
 	console.log("switching to theme " + theme_name);
 	switch(theme_name) {
 		case "halloween":
-			document.getElementById('style-halloween').disabled=false;
+			document.getElementById('base-styles').disabled = false;
+			document.getElementById('style-halloween').disabled = false;
 			document.getElementById('style-dark').disabled = true;
 			document.getElementById('style-light').disabled = true;
 			break;
 		case "dark":
-			document.getElementById('style-halloween').disabled=true;
+			document.getElementById('base-styles').disabled = false;
+			document.getElementById('style-halloween').disabled = true;
 			document.getElementById('style-dark').disabled = false;
+			document.getElementById('style-light').disabled = true;
+			break;
+		case "naked":
+			document.getElementById('base-styles').disabled = true;
+			document.getElementById('style-halloween').disabled = true;
+			document.getElementById('style-dark').disabled = true;
 			document.getElementById('style-light').disabled = true;
 			break;
 		case "light":
 		default:
+			document.getElementById('base-styles').disabled = false;
 			document.getElementById('style-halloween').disabled=true;
 			document.getElementById('style-dark').disabled = true;
 			document.getElementById('style-light').disabled = false;
@@ -33,6 +42,14 @@ if (localStorage.getItem('current-theme') !== undefined && localStorage.getItem(
 	console.log("theme preference not set but dark mode preferred, using dark theme");
 	theme_set("dark");
 } else {
-	console.log("no previous theme preference and no dark mode preference, using light theme");
-	theme_set("light");
+	const date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	if (day == 9 && month == 4) {
+	console.log("theme preference not set and no dark mode preference, and it's css naked day!");
+	theme_set("naked");
+	} else {
+		console.log("no previous theme preference and no dark mode preference, using light theme");
+		theme_set("light");
+	}
 }
